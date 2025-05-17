9915924dfe9ab94b90460a690fa8bc272119d5e5
ttmatteus <matteusgn@gmail.com>
2025-05-17 18:19:50 -0300

    feat(db): add PostgreSQL configuration using DATABASE_URL environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
 import { LinhaModule } from './linha/linha.module';
 import { RotaModule } from './rota/rota.module';
 import { OnibusModule } from './onibus/onibus.module';
@@ -7,8 +9,19 @@ import { SimuladorModule } from './simulador/simulador.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [LinhaModule, RotaModule, OnibusModule, SimuladorModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService): TypeOrmModuleOptions => {
+      return config.get<TypeOrmModuleOptions>('database')!;
+    },
+      inject: [ConfigService],
+    }),
+    LinhaModule, RotaModule, OnibusModule, SimuladorModule, AuthModule
+  ],
 })
 export class AppModule {}
