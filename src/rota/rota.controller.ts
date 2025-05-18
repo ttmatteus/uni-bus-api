7ea3a8d675e7a9096a43fb1e427e4459b877ad5e
ttmatteus <matteusgn@gmail.com>
2025-05-17 23:33:19 -0300

    feat(rota): implement basic CRUD for Rota module

--- src/rota/rota.controller.ts
+++ src/rota/rota.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { RotaService } from './rota.service';
+import { CreateRotaDto } from './dto/create-rota.dto';
+import { UpdateLinhaDto } from 'src/linha/dto/update-linha.dto';
 
 @Controller('rota')
-export class RotaController {}
+export class RotaController {
+    constructor(private readonly rotaService: RotaService){}
+
+    @Post()
+    create(@Body() dto: CreateRotaDto) {
+        return this.rotaService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.rotaService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.rotaService.findOne(+id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateLinhaDto) {
+        return this.rotaService.update(+id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.rotaService.remove(+id);
+    }
+}
--- src/rota/rota.service.ts
+++ src/rota/rota.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Rota } from './rota.entity';
+import { CreateRotaDto } from './dto/create-rota.dto';
+import { UpdateLinhaDto } from 'src/linha/dto/update-linha.dto';
+import { Linha } from 'src/linha/linha.entity';
 
 @Injectable()
-export class RotaService {}
+export class RotaService {
+    constructor(
+        @InjectRepository(Rota)
+        private rotaRepository: Repository<Rota>,
+
+        @InjectRepository(Linha)
+        private linhaRepository: Repository<Linha>,
+    ) {}
+
+    async create(dto: CreateRotaDto) {
+        const linha = await this.linhaRepository.findOne({ where: { id: dto.linha_id } });
+
+        if (!linha) {
+            throw new NotFoundException('Linha não encontrada');
+        }
+
+        const novaRota = this.rotaRepository.create({
+            nome: dto.nome,
+            linha,
+            pontos: dto.pontos
+        });
+
+        return this.rotaRepository.save(novaRota);
+    }
+
+    findAll() {
+        return this.rotaRepository.find({
+            relations: ['linha']
+        });
+    }
+
+    findOne(id: number) {
+        return this.rotaRepository.findOne({
+            where: { id },
+            relations: ['linha'],
+        });
+    }
+
+    update(id: number, dto: UpdateLinhaDto) {
+        return this.rotaRepository.update(id, dto);
+    }
+
+    remove(id: number) {
+        return this.rotaRepository.delete(id);
+    }
+}
